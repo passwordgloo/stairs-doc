94d484eb6d1b15fe73591a6aa5cb0bb232b439fb
passwordgloo <passwordgloo@163.com>
2024-06-01 09:58:26 +0800

    revert(theme-reco): git 忽略掉dist，需要纳入推送
    合并分支不是一个好办法，现阶段凑合用
    
    ISSUES CLOSED: #1

---
+++ docs/.vuepress/dist/assets/demo-DoIqw5VU.js
@@ -0,0 +1,1 @@
+import{f as n,o as t,c as a,b as e,t as c,h as _,_ as r}from"./app-BwIZjAV-.js";function i(){return{name:"reco"}}const m={class:"demo-container"},d={style:{}},p=e("p",null,"This is a vue preview demo.",-1),u=n({__name:"demo",setup(o){const{name:s}=i();return(f,l)=>(t(),a("div",m,[e("h3",d,"My name is "+c(_(s))+".",1),p]))}}),v=r(u,[["__file","demo.vue"]]);export{v as default};
